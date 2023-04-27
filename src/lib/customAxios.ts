46219330f0812f26f66f552a933db7eb2291c7d1
YejinHwang-D <hyj3463@naver.com>
2023-04-27 15:38:16 +0900

    feat: axios 인스턴스 생성  및 안내 메시지 상수화

--- src/lib/customAxios.ts
+++ src/lib/customAxios.ts
@@ -1,1 +1,19 @@
-// write your code.
+import axios from "axios";
+import constant from "../constant/constant.json";
+
+const customAxios = axios.create({
+  baseURL: constant.SERVER,
+});
+
+customAxios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
