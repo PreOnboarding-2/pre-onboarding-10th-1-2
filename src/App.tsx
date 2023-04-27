781c26deaed414520aa92c2c003c0a76f5cee47a
LuisKlopp <fbgus333@gmail.com>
2023-04-27 18:03:07 +0900

    chore: styled-components 및 axios 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
 function App() {
-  return <div className="App">Welcome</div>;
+  return <Container className="App">Welcome</Container>;
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+`;
 
 export default App;
