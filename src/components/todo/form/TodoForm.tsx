baf2e1fd75c549425b905397a9a6a2aa1752cfd1
강세민 <ksm9802@gmail.com>
2023-04-27 22:15:45 +0900

    design: 투두리스트 관련 컴포넌트
    * 구조 설명
      * form/: 새로운 투두리스트 추가에 관련된 컴포넌트 및 스타일을 모아둔 폴더
      * item/: 각 투두리스트 항목에 관련된 컴포넌트 및 스타일을 모아둔 폴더
      * TodoList.tsx: 각 투두리스트들이 모여 만들어진 리스트에 관련된 컴포넌트
    
    * 특이사항
      * 스타일
        - todoItem.style.ts에서 span에 word-wrap으로 break-word 사용 -> 투두리스트 항목의 글자수가 많아져도 레이아웃이 깨지지 않도록 함
      * TodoForm.tsx에서 가장 상위 HTML이 div가 아닌 form이 되도록 해, HTML 구조를 덜 깊게할 수 있도록 함

---
+++ src/components/todo/form/TodoForm.tsx
@@ -0,0 +1,25 @@
+import StyledForm from "./todoForm.style";
+import Input from "../../common/input/Input";
+import { useTodoForm } from "../../../hooks/todo/useTodoForm";
+import Button from "../../common/button/Button";
+
+const TodoForm = () => {
+  const { newTodo, newTodoInputRef, handleSubmit, handleChange } = useTodoForm();
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <Input
+        value={newTodo}
+        ref={newTodoInputRef}
+        type={"text"}
+        name="new-todo"
+        data-testid="new-todo-input"
+        onChange={handleChange}
+      />
+      <Button type="submit" data-testid="new-todo-add-button">
+        추가
+      </Button>
+    </StyledForm>
+  );
+};
+
+export default TodoForm;
