c026aeba41ee4682554bc29ccbd44805885fd15a
YejinHwang-D <hyj3463@naver.com>
2023-04-28 00:00:41 +0900

    fix: 오타 수정

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -2,26 +2,26 @@ import customAxios from "../lib/customAxios";
 import { SigninResponse, SigninParam, SignupParam } from "./types/auth/index";
 import CONSTANT from "../constant/constant.json";
 
-const { SIGNUP_SUCESS, SIGNUP_ERROR, SIGNIN_ERROR, SIGNIN_SUCCESS } = CONSTANT;
+const { SIGNUP_SUCCESS, SIGNUP_ERROR, SIGNIN_ERROR, SIGNIN_SUCCESS } = CONSTANT;
 
 const AuthApi = () => {
   const signIn = async (formData: SigninParam): Promise<SigninResponse> => {
     const result = await customAxios.post("/auth/signin", formData);
     if (result.status === 201) {
-      alert(SIGNUP_SUCESS);
+      alert(SIGNIN_SUCCESS);
       return result.data;
     } else {
-      alert(SIGNUP_ERROR);
+      alert(SIGNIN_ERROR);
     }
   }
 
   const signUp = async (formData: SignupParam): Promise<void> => {
     const result = await customAxios.post("/auth/signup", formData);
     if (result.status === 200) {
-      alert(SIGNIN_SUCCESS);
+      alert(SIGNUP_SUCCESS);
       return result.data;
     } else {
-      alert(SIGNIN_ERROR);
+      alert(SIGNUP_ERROR);
     }
   }
 
