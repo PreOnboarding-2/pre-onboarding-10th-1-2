1a0b361ea00540109a17343d495b3f62ea3e5e10
YejinHwang-D <hyj3463@naver.com>
2023-04-27 23:28:09 +0900

    update: auth 관련 타입 추가

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -1,33 +1,27 @@
-// write your code.
 import customAxios from "../lib/customAxios";
+import { SigninResponse, SigninParam, SignupParam } from "./types/auth/index";
 import CONSTANT from "../constant/constant.json";
 
 const { SIGNUP_SUCESS, SIGNUP_ERROR, SIGNIN_ERROR, SIGNIN_SUCCESS } = CONSTANT;
 
 const AuthApi = () => {
-  const signUp = async (formData) => {
-    try {
-      const result = await customAxios.post("/auth/signin", formData);
-      if (result.status === 201) {
-        alert(SIGNUP_SUCESS);
-      } else {
-        alert(SIGNUP_ERROR);
-      }
-    } catch (error) {
-      error
+  const signIn = async (formData: SigninParam): Promise<SigninResponse> => {
+    const result = await customAxios.post("/auth/signin", formData);
+    if (result.status === 201) {
+      alert(SIGNUP_SUCESS);
+      return result.data;
+    } else {
+      alert(SIGNUP_ERROR);
     }
   }
 
-  const signIn = async (formData) => {
-    try {
-      const result = await customAxios.post("/auth/signup", formData);
-      if (result.status === 200) {
-        alert(SIGNIN_SUCCESS);
-      } else {
-        alert(SIGNIN_ERROR);
-      }
-    } catch (error) {
-      error
+  const signUp = async (formData: SignupParam): Promise<void> => {
+    const result = await customAxios.post("/auth/signup", formData);
+    if (result.status === 200) {
+      alert(SIGNIN_SUCCESS);
+      return result.data;
+    } else {
+      alert(SIGNIN_ERROR);
     }
   }
 
