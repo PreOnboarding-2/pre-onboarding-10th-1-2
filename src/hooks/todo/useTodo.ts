24d0e2ecbae373b3820b7c08c2e2d4fd16ed6cd1
sohan <sohan@student.42seoul.kr>
2023-04-28 02:56:53 +0900

    fix: props로 훅을 전달 해도 상태 공유 안돼서 훅들을 useTodo 하나로 합침.

--- src/hooks/todo/useTodo.ts
+++ src/hooks/todo/useTodo.ts
@@ -4,6 +4,14 @@ import TodoApi from "../../api/TodoApi";
 
 const useTodo = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [isModifyMode, setIsModifyMode] = useState<Todo | null>(null);
+  const [modifyTodoInput, setModifyTodoInput] = useState("");
+  const newTodoInputRef = useRef<HTMLInputElement>(null);
+
+  const initTodos = (todos: Todo[]) => {
+    setTodos(todos);
+  };
 
   const createTodo = (newTodo: Todo) => {
     setTodos([...todos, newTodo]);
@@ -21,11 +29,88 @@ const useTodo = () => {
   const getTodos = useCallback(async () => {
     try {
       const data = await TodoApi.getTodos();
-      setTodos(data);
+      initTodos(data);
     } catch (error) {
       alert("todolist를 가져오는데 실패하였습니다");
     }
-  }, [setTodos]);
+  }, [initTodos]);
+
+  const handleTodoFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleTodoFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    try {
+      event.preventDefault();
+      if (newTodo.trim() !== "") {
+        const data = await TodoApi.createTodo(newTodo);
+        createTodo(data);
+      }
+      setNewTodo("");
+    } catch (error) {
+      alert("todo 추가를 실패하였습니다.");
+    }
+  };
+
+  const modifyTodoHandler = async (item: Todo) => {
+    try {
+      const modifiedTodo = await TodoApi.modifyTodo({
+        todoId: item.id,
+        isCompleted: item.isCompleted,
+        todo: item.todo,
+      });
+      modifyTodo(modifiedTodo);
+    } catch (error) {
+      alert("todo수정을 실패하였습니다.");
+    }
+  };
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setModifyTodoInput(event.target.value);
+  };
+
+  const handleModifyTodo = async (item: Todo) => {
+    try {
+      if (modifyTodoInput.trim() !== "") {
+        const modifiedTodo = await TodoApi.modifyTodo({
+          todoId: item.id,
+          isCompleted: item.isCompleted,
+          todo: modifyTodoInput,
+        });
+        modifyTodo(modifiedTodo);
+        setIsModifyMode(null);
+      }
+    } catch (error) {
+      alert("todo수정을 실패하였습니다.");
+    }
+  };
+
+  const handleChangeModifyIsCompleted = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    item: Todo
+  ) => {
+    const { checked } = event.target;
+    modifyTodoHandler({ ...item, isCompleted: checked });
+  };
+
+  const handleOnModifyMode = (item: Todo) => {
+    setIsModifyMode(item);
+    setModifyTodoInput(item.todo);
+  };
+
+  const handleCancelModfyMode = (item: Todo) => {
+    setModifyTodoInput(item.todo);
+    setIsModifyMode(null);
+  };
+
+  const handleDeleteTodo = async (item: Todo) => {
+    try {
+      await TodoApi.deleteTodo(item.id);
+      deleteTodo(item.id);
+    } catch (error) {
+      alert("todo삭제를 실패하였습니다");
+    }
+  };
 
   useEffect(() => {
     getTodos();
@@ -33,9 +118,18 @@ const useTodo = () => {
 
   return {
     todos,
-    createTodo,
-    modifyTodo,
-    deleteTodo,
+    newTodo,
+    newTodoInputRef,
+    handleTodoFormChange,
+    handleTodoFormSubmit,
+    isModifyMode,
+    modifyTodoInput,
+    handleModifyTodo,
+    handleChangeInput,
+    handleChangeModifyIsCompleted,
+    handleOnModifyMode,
+    handleCancelModfyMode,
+    handleDeleteTodo,
   };
 };
 
