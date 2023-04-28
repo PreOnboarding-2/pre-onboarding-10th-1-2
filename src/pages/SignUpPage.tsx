2ad6b29ea18702a2a50be7f6c2b950519a4c86e6
LuisKlopp <fbgus333@gmail.com>
2023-04-28 13:06:07 +0900

    feat: sign페이지내에 signForm생성

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import SignForm from "../components/common/form/SignForm";
+import useSignUp from "../hooks/auth/useSignUp";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const { userInfo, handleSubmit, handleChange, isValid } = useSignUp();
+  return (
+    <div>
+      <SignForm
+        isValid={isValid}
+        value={userInfo}
+        title="회원가입"
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+      />
+      <Link to={"/signin"}>로그인 하러 가기</Link>
+    </div>
+  );
 };
 
 export default SignUpPage;
