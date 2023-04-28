da8e75aa06e0e2880ed6125c00615d3d07feaacb
Song ji su <thdwltn0727@gmail.com>
2023-04-28 19:34:27 +0900

    fix : emailinput으로 자동 포커싱 되는 현상 해결

--- src/hooks/form/useForm.ts
+++ src/hooks/form/useForm.ts
@@ -4,7 +4,7 @@ export const useForm = () => {
   const emailRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    // debounce
+    
     const debounce = (func: any, delay: number) => {
       let timeoutId: ReturnType<typeof setTimeout>;
       return (...args: any[]) => {
@@ -15,24 +15,16 @@ export const useForm = () => {
       };
     };
 
-    // email input 엘리먼트에 focus
-    const focusEmailRef = () => {
-      emailRef.current?.focus();
-    };
-
     // delay 500ms
-    const debouncedFocusEmailRef = debounce(focusEmailRef, 500);
+    const debouncedFocusEmailRef = debounce(() => {}, 500);
 
     const handleInput = () => {
       debouncedFocusEmailRef();
     };
 
-    // document 객체에 input 이벤트 리스너 등록
     document.addEventListener("input", handleInput);
 
-    // useEffect 함수의 cleanup 함수
     return () => {
-      // document 객체에서 input 이벤트 리스너 제거
       document.removeEventListener("input", handleInput);
     };
   }, []);
