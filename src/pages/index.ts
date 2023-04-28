3d8ca9f211f472185c0045d83eab104e7c9f778c
ad105geppetto <ad105geppetto@gmail.com>
2023-04-28 13:43:06 +0900

    chore: lint 적용

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,8 +3,7 @@ import SignInPage from "./SignInPage";
 import SignUpPage from "./SignUpPage";
 import TodoPage from "./TodoPage";
 
-
-export { default as NotFoundPage } from "./404Page"
-export const AuthSignInPage = AuthHOC(SignInPage, false)
-export const AuthSignUpPage = AuthHOC(SignUpPage, false)
-export const AuthTodoPage = AuthHOC(TodoPage, true)
+export { default as NotFoundPage } from "./404Page";
+export const AuthSignInPage = AuthHOC(SignInPage, false);
+export const AuthSignUpPage = AuthHOC(SignUpPage, false);
+export const AuthTodoPage = AuthHOC(TodoPage, true);
